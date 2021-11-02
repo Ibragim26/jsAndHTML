3606daaedd5bec2d0e499f77351e382cdf040a19
LanceSunn <ibragim26061997@gmail.com>
2021-11-02 16:34:50 +0300

    version 0.2

--- scr.js
+++ scr.js
@@ -1,5 +1,37 @@
 
 
+// const PHONE_NUMBER = {
+//     'Пожарная служба': [
+//         ['num1', '101'],
+//         ['num2', '102'],
+//         ['num3', '103'],
+//     ],
+//     'Полиция': [
+//         ['num1', '101'],
+//         ['num2', '202']
+//     ],
+//     'Скорая помощь': [
+//         ['num1', '03']
+//     ]
+// }
+//
+// document.querySelector('.wrapper').innerHTML =
+//     `<table class="phone"> </table>`
+// for (key in PHONE_NUMBER){
+//     let row = document.createElement('tr')
+//     row.innerHTML = `<td colspan="2">${key}</td>`
+//     document.querySelector('.wrapper').appendChild(row)
+//     for (let i = 0; i < PHONE_NUMBER[key].length; i++){
+//         let row = document.createElement('tr')
+//         row.innerHTML = `
+//             <td>${PHONE_NUMBER[key][i][0]}</td>
+//             <td>${PHONE_NUMBER[key][i][1]}</td>
+//         `
+//         document.querySelector('.wrapper').appendChild(row)
+//     }
+// }
+
+
 const person = {
     name: 'Ivan',
     age: 20,
@@ -8,7 +40,7 @@ const person = {
 }
 
 function dynTable(arr) {
-    document.querySelector('body')
+    document.querySelector('.dynTa')
         .innerHTML = `<table class="tab"></table>`
     for (let i = 0; i < arr.length; i++) {
         let tr = document.createElement('tr');
@@ -22,3 +54,4 @@ function dynTable(arr) {
 }
 
 dynTable(Object.entries(person))
+
